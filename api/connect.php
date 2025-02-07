a47448fea869a2abe768ca0604e359643cb4d877
Ezekiel Samuel <142183985+samkiel488@users.noreply.github.com>
2025-02-07 05:15:54 +0100

    Update connect.php

--- api/connect.php
+++ api/connect.php
@@ -1,3 +1,17 @@
 <?php
-echo "API is working!";
+header('Content-Type: application/json');
+
+try {
+    $uri = "mongodb+srv://devspace:devspace@samkiel.ij9vc8f.mongodb.net/?retryWrites=true&w=majority&appName=SAMKIEL";
+    echo json_encode([
+        "status" => "success",
+        "message" => "Testing MongoDB URI",
+        "uri" => $uri
+    ]);
+} catch (Exception $e) {
+    echo json_encode([
+        "status" => "error",
+        "message" => $e->getMessage()
+    ]);
+}
 ?>
