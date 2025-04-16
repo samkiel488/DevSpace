c3b7447618337ebfe97f693397beac86980a9e4f
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-16 04:07:50 +0100

    Update About Us section styling and image source for consistency

--- frontend/src/componet/Home/aboutUs.jsx
+++ frontend/src/componet/Home/aboutUs.jsx
@@ -1,6 +1,6 @@
 export default function AboutUs() {
   return (
-    <div className="overflow-hidden bg-white py-24 sm:py-32">
+    <div className="overflow-hidden bg-white py-24 sm:py-10">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pt-4 lg:pr-8">
@@ -9,15 +9,22 @@ export default function AboutUs() {
                 About D3V++ Group
               </p>
               <p className="mt-6 text-lg/8 text-gray-600">
-              D3V++ Group is a growing community of young developers passionate about coding, creativity, and connection. We're building a space where devs at all levels can learn, grow, and collaborate on real-world projects that matter.
-From coding challenges and group projects to mentorship and networking, we focus on helping each other level up—together. Whether you're into front-end, back-end, mobile, or just starting out, there's a place for you here.
-This is more than a tech group. It's a launchpad for future innovators.Welcome to D3V++ Group—let’s build something awesome.
+                D3V++ Group is a growing community of young developers
+                passionate about coding, creativity, and connection. We're
+                building a space where devs at all levels can learn, grow, and
+                collaborate on real-world projects that matter. From coding
+                challenges and group projects to mentorship and networking, we
+                focus on helping each other level up—together. Whether you're
+                into front-end, back-end, mobile, or just starting out, there's
+                a place for you here. This is more than a tech group. It's a
+                launchpad for future innovators.Welcome to D3V++ Group—let’s
+                build something awesome.
               </p>
             </div>
           </div>
           <img
             alt="Product screenshot"
-            src="https://tailwindcss.com/plus-assets/img/component-images/dark-project-app-screenshot.png"
+            src="/images/aboutUs-bg.jpeg"
             width={2432}
             height={1442}
             className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
