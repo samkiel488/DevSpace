3ac6ae3bd10262304d5190219d59541a316fd178
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-16 08:27:50 +0100

    Implement dark mode toggle functionality in App component

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,9 +1,10 @@
 import Header from "../componet/header";
 import AboutUs from "../componet/Home/aboutUs";
-
+import { useState } from "react";
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <>
+    <div data-theme={darkMode ? "dark" : "light"}>
       <div className="relative bg-[url('/images/hero-bg.jpg')] bg-cover bg-center bg-no-repeat">
         <Header colorLightMode="transparent" colorDarkMode="transparent" />
         <main className="relative isolate px-6 pt-12 pb-20 lg:px-8 text-white text-center z-10">
@@ -31,6 +32,42 @@ export default function App() {
         </main>
       </div>
       <AboutUs />
-    </>
+      <button
+        className="fixed bottom-6 right-6 z-50 rounded-full bg-black text-white dark:bg-white dark:text-black p-3 shadow-lg transition duration-300 hover:cursor-pointer"
+        onClick={() => setDarkMode(!darkMode)}
+      >
+        {darkMode ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            />
+          </svg>
+        )}
+      </button>
+    </div>
   );
 }
