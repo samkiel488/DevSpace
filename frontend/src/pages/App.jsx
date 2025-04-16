3bc7df3f296289917a5eb9d1ab1c0e07c5fe64cf
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-16 03:36:51 +0100

    Fix background image filename and adjust text styling for improved readability

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -2,7 +2,7 @@ import Header from "../componet/header";
 
 export default function App() {
   return (
-    <div className="relative bg-[url('/images/hero-bg.jpeg')] bg-cover bg-center bg-no-repeat">
+    <div className="relative bg-[url('/images/hero-bg.jpg')] bg-cover bg-center bg-no-repeat">
       <Header colorLightMode="transparent" colorDarkMode="transparent" />
       <main className="relative isolate px-6 pt-12 pb-20 lg:px-8 text-white text-center z-10">
         <div className="mx-auto max-w-3xl pt-10">
@@ -12,7 +12,7 @@ export default function App() {
           <h1 className="text-2l pt-5 tracking-tight font-medium text-white sm:text-2xl">
             A space where young devs grow, connect, and build cool stuff
           </h1>
-          <p className="mt-8 text-lg font-light text-white/80 sm:text-xl">
+          <p className="mt-8 text-lg tracking-tight font-bold text-white/80 sm:text-xl">
             Whether you're just getting started or leveling up your
             skills,you're in the right place. Collaborate on real projects,
             share ideas, and build a future fueled by code and creativity.
