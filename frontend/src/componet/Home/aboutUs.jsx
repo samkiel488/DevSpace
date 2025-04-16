a13e2bbbc8efccbfd300f02552a70c201ec3edf1
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-16 09:07:47 +0100

    Update dark mode background color in AboutUs component

--- frontend/src/componet/Home/aboutUs.jsx
+++ frontend/src/componet/Home/aboutUs.jsx
@@ -1,6 +1,6 @@
 export default function AboutUs() {
   return (
-    <div className="overflow-hidden bg-white  dark:bg-slate-900 text-black dark:text-white py-24 sm:py-10">
+    <div className="overflow-hidden bg-white  dark:bg-zinc-800 text-black dark:text-white py-24 sm:py-10">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pt-4 lg:pr-8">
