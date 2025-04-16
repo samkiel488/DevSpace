09a0104c4657f037cc450748097c8f90d2c3089a
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-16 01:21:56 +0100

    Add CORS support to Express server for cross-origin requests

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,21 @@
 import express from "express";
+import cors from "cors";
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 const app = express();
 const PORT = 8080;
 
-app.get("/api", (req , res)=>{
-    res.json({message: "Hello from the server!"});
-})
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from the server!" });
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port localhost:${PORT}`);
+});
