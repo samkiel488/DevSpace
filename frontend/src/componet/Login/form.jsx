54ef61b4c931aa4444bffcaf9489c7c5c67901e3
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-17 00:05:58 +0100

    Add password visibility toggle and update styles in Form component

--- frontend/src/componet/Login/form.jsx
+++ frontend/src/componet/Login/form.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div class="min-h-screen bg-white  flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-[url('/images/hero-bg.jpeg')] bg-cover bg-center bg-no-repeat">
       <div class="sm:mx-auto sm:w-full sm:max-w-md">
@@ -12,7 +15,7 @@ export default function Form() {
             </p>
           </div>
 
-          <form class="space-y-6">
+          <form class="space-y-6" action="">
             <div>
               <label
                 class="block text-sm font-medium text-gray-700"
@@ -37,7 +40,7 @@ export default function Form() {
               </label>
               <div class="mt-1 relative">
                 <input
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   id="password"
                   class="block w-full px-3 py-2 sm:px-4 sm:py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 focus:outline-blue-500"
                   required
@@ -45,39 +48,45 @@ export default function Form() {
                 <button
                   type="button"
                   class="absolute inset-y-0 right-0 pr-3 flex items-center"
+                  onClick={() => {
+                    setShowPassword(!showPassword);
+                  }}
                 >
-                  <svg
-                    class="h-5 w-5 text-gray-400"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                    />
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                    />
-                  </svg>
-                  <svg
-                    class="h-5 w-5 text-gray-400"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
-                    />
-                  </svg>
+                  {showPassword ? (
+                    <svg
+                      class="h-5 w-5 text-gray-400"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      class="h-5 w-5 text-gray-400"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                      />
+                    </svg>
+                  )}
                 </button>
               </div>
             </div>
@@ -87,13 +96,13 @@ export default function Form() {
                 <input
                   type="checkbox"
                   id="remember"
-                  class="h-4 w-4 text-red-600 focus:ring-red-500 border-gray-300 rounded"
+                  class="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
                 />
                 <label for="remember" class="ml-2 block text-sm text-gray-700">
                   Remember me
                 </label>
               </div>
-              <a href="#" class="text-sm text-red-600 hover:text-red-700">
+              <a href="#" class="text-sm text-blue-600 hover:text-blue-700">
                 Forgot password?
               </a>
             </div>
@@ -107,7 +116,7 @@ export default function Form() {
 
           <p class="mt-6 text-center text-sm">
             Don't have an account?
-            <a href="#" class="font-medium text-red-600 hover:text-red-700">
+            <a href="#" class="font-medium text-blue-600 hover:text-blue-700">
               Sign up now
             </a>
           </p>
