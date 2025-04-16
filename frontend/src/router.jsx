5c5d67cc810aaad1d3d01bc5771d147e22bbfb42
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-16 23:53:18 +0100

    Add route for Login component in AppRouter

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import App from "./pages/App";
+import Login from "./pages/login";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
