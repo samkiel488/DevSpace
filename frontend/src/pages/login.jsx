d0331c0aba3a9c720bdb9a99426cd3887433ad6f
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-16 23:53:25 +0100

    Remove Toggle component and dark mode state from Login page

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,16 +1,13 @@
 import Footer from "../componet/footer";
 import Header from "../componet/header";
 import Form from "../componet/Login/form";
-import Toggle from "../componet/toggle";
-import { useState } from "react";
+
 export default function Login() {
-  const [darkMode, setDarkMode] = useState(false);
   return (
-    <div data-theme={darkMode ? "dark" : "light"}>
+    <div>
       <Header colorLightMode="black" colorDarkMode="black" />
       <Form />
       <Footer />
-      <Toggle darkMode={darkMode} setDarkMode={setDarkMode} />
     </div>
   );
 }
