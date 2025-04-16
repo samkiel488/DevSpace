9c7f4e73e37f4ddb608ae0da5120746eab125a9e
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-16 03:57:12 +0100

    Implement AboutUs component layout with content and styling

--- frontend/src/componet/Home/aboutUs.jsx
+++ frontend/src/componet/Home/aboutUs.jsx
@@ -1,3 +1,28 @@
 export default function AboutUs() {
-    return()
+  return (
+    <div className="overflow-hidden bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pt-4 lg:pr-8">
+            <div className="lg:max-w-lg">
+              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
+                A better workflow
+              </p>
+              <p className="mt-6 text-lg/8 text-gray-600">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
+                iste dolor cupiditate blanditiis ratione.
+              </p>
+            </div>
+          </div>
+          <img
+            alt="Product screenshot"
+            src="https://tailwindcss.com/plus-assets/img/component-images/dark-project-app-screenshot.png"
+            width={2432}
+            height={1442}
+            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
