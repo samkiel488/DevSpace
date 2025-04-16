7d4ccb9372a82d56285f99458b242d3319a46fba
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-16 08:27:55 +0100

    Enhance About Us section with dark mode support and improve text styling

--- frontend/src/componet/Home/aboutUs.jsx
+++ frontend/src/componet/Home/aboutUs.jsx
@@ -1,14 +1,14 @@
 export default function AboutUs() {
   return (
-    <div className="overflow-hidden bg-white py-24 sm:py-10">
+    <div className="overflow-hidden bg-white  dark:bg-slate-900 text-black dark:text-white py-24 sm:py-10">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pt-4 lg:pr-8">
             <div className="lg:max-w-lg">
-              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
+              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty sm:text-5xl">
                 About D3V++ Group
               </p>
-              <p className="mt-6 text-lg/8 text-gray-600">
+              <p className="mt-6 text-lg/8">
                 D3V++ Group is a growing community of young developers
                 passionate about coding, creativity, and connection. We're
                 building a space where devs at all levels can learn, grow, and
