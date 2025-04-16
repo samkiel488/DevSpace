6e43b7f21ef62f5ac986f1236ba43ad86ab004c0
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-16 01:47:29 +0100

    Refactor index.js to remove duplicate variable declarations and improve code organization

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,13 @@
 import express from "express";
 import cors from "cors";
-
+const app = express();
+const PORT = 8080;
 const corsOptions = {
   origin: "http://localhost:5173",
   methods: ["GET", "POST"],
-  credentials: true,
 };
 
 app.use(cors(corsOptions));
-
-const app = express();
-const PORT = 8080;
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from the server!" });
