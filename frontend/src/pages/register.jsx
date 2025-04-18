6c54b6a115fd6fb983734f0af9683389adc93eac
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-18 21:20:31 +0100

    Add RegisterForm component to Register page

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,9 +1,11 @@
 import Header from "../Componets/header";
 import Footer from "../Componets/footer";
+import RegisterForm from "../Componets/Register/registerForm";
 export default function Register() {
   return (
     <>
       <Header colorLightMode="black" colorDarkMode="black" />
+      <RegisterForm />
       <Footer />
     </>
   );
