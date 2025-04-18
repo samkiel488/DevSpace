748054ee181daa22b6f7cd090ed61c7752e6f2d9
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-18 19:57:09 +0100

    Add Footer component to Register page

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,9 +1,10 @@
 import Header from "../Componets/header";
-
+import Footer from "../Componets/footer";
 export default function Register() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
