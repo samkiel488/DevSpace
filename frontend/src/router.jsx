973c3e00bac718184f697353244faa718f4b3fe1
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-18 19:57:13 +0100

    Add route for Register page in AppRouter

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import App from "./pages/App";
 import Login from "./pages/login";
-
+import Register from "./pages/register";
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register/>}/>
     </Routes>
   );
 }
