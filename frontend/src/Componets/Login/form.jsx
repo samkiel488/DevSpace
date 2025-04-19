9b482d7f27da65aba60863ca89eba55def37e798
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-19 20:49:09 +0100

    Add name attribute to password input in Form component

--- frontend/src/Componets/Login/form.jsx
+++ frontend/src/Componets/Login/form.jsx
@@ -67,6 +67,7 @@ export default function Form() {
                 <input
                   type={showPassword ? "text" : "password"}
                   id="password"
+                  name="password"
                   className="block w-full px-3 py-2 sm:px-4 sm:py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 focus:outline-blue-500"
                   required
                 />
