38e7cccbda1b7eda7c3ae742e5e56c6b41b0052b
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-19 22:28:59 +0100

    Fix inconsistent property naming in emailAddress array

--- frontend/src/Componets/defaultUser.js
+++ frontend/src/Componets/defaultUser.js
@@ -1,24 +1,27 @@
-
 const emailAddress = [
   {
     fName: "Lara",
     lName: "James",
     gmailAddress: "j@gmail.com",
+    userName: "jamine",
   },
   {
     fName: "David",
     lName: "Olawale",
     gmailAddress: "d@gmail.com",
+    userName: "davido",
   },
   {
     fName: "Chioma",
     lName: "Nwosu",
     gmailAddress: "c@gmail.com",
+    username: "chiom",
   },
   {
     fName: "Tunde",
     lName: "Bamidele",
     gmailAddress: "t@gmail.com",
+    userName: "bami",
   },
 ];
 
