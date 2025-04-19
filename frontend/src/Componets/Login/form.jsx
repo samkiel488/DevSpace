283c95d278eb2393c48ddac0d0f0216b73c090c6
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-19 12:54:00 +0100

    Update signup link in Form component to direct to the registration page

--- frontend/src/Componets/Login/form.jsx
+++ frontend/src/Componets/Login/form.jsx
@@ -142,7 +142,7 @@ export default function Form() {
           <p className="mt-6 text-center text-sm">
             Don't have an account?
             <a
-              href="#"
+              href="/register"
               className="font-medium text-blue-600 hover:text-blue-700"
             >
               Sign up now
