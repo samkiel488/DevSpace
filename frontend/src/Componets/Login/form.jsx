04b430ae96120ed28fe6ad0c7d8e8ae97ec9ab33
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-19 12:43:31 +0100

    Refactor email address handling by importing from defaultUser module

--- frontend/src/Componets/Login/form.jsx
+++ frontend/src/Componets/Login/form.jsx
@@ -1,26 +1,5 @@
 import { useState } from "react";
-const emailAddress = [
-  {
-    fName: "Lara",
-    lName: "James",
-    gmailAddress: "j@gmail.com",
-  },
-  {
-    fName: "David",
-    lName: "Olawale",
-    gmailAddress: "d@gmail.com",
-  },
-  {
-    fName: "Chioma",
-    lName: "Nwosu",
-    gmailAddress: "c@gmail.com",
-  },
-  {
-    fName: "Tunde",
-    lName: "Bamidele",
-    gmailAddress: "t@gmail.com",
-  },
-];
+import emailAddress from "../defaultUser";
 
 export default function Form() {
   var [showPassword, setShowPassword] = useState(false);
