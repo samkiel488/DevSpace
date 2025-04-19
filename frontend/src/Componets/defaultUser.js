e1cb3131de9bd4e20f956fc2e0af0b44a5bd405e
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-19 12:43:26 +0100

    Add default user email addresses for application functionality

---
+++ frontend/src/Componets/defaultUser.js
@@ -0,0 +1,24 @@
+const emailAddress = [
+  {
+    fName: "Lara",
+    lName: "James",
+    gmailAddress: "j@gmail.com",
+  },
+  {
+    fName: "David",
+    lName: "Olawale",
+    gmailAddress: "d@gmail.com",
+  },
+  {
+    fName: "Chioma",
+    lName: "Nwosu",
+    gmailAddress: "c@gmail.com",
+  },
+  {
+    fName: "Tunde",
+    lName: "Bamidele",
+    gmailAddress: "t@gmail.com",
+  },
+];
+
+export default emailAddress;
