c2f1613482fad03b9fa8c7074fc6628f2abbd6e2
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-24 00:47:50 +0100

    Update OurTeam component styles for improved accessibility and visual consistency

--- frontend/src/Componets/Home/ourTeam.jsx
+++ frontend/src/Componets/Home/ourTeam.jsx
@@ -14,18 +14,19 @@ export default function OurTeam() {
                 alt="team"
                 class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
                 src="https://dummyimage.com/200x200"
+                style={{ width: "200px" }}
               />
               <div class="flex-grow sm:pl-8">
-                <h2 class="title-font font-medium text-lg text-gray-900">
+                <h2 class="title-font font-medium text-lg text-black dark:text-white">
                   Holden Caulfield
                 </h2>
-                <h3 class="text-gray-500 mb-3">UI Developer</h3>
+                <h3 class="text-black dark:text-white mb-3">UI Developer</h3>
                 <p class="mb-4">
                   DIY tote bag drinking vinegar cronut adaptogen squid fanny
                   pack vaporware.
                 </p>
                 <span class="inline-flex">
-                  <a class="text-gray-500">
+                  <a class="text-black dark:text-white">
                     <svg
                       fill="none"
                       stroke="currentColor"
@@ -38,7 +39,7 @@ export default function OurTeam() {
                       <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                     </svg>
                   </a>
-                  <a class="ml-2 text-gray-500">
+                  <a class="ml-2 text-black dark:text-white">
                     <svg
                       fill="none"
                       stroke="currentColor"
@@ -51,7 +52,7 @@ export default function OurTeam() {
                       <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                     </svg>
                   </a>
-                  <a class="ml-2 text-gray-500">
+                  <a class="ml-2 text-black dark:text-white">
                     <svg
                       fill="none"
                       stroke="currentColor"
@@ -68,7 +69,7 @@ export default function OurTeam() {
               </div>
             </div>
           </div>
-          <div class="p-4 lg:w-1/2">
+          {/* <div class="p-4 lg:w-1/2">
             <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
               <img
                 alt="team"
@@ -247,7 +248,7 @@ export default function OurTeam() {
                 </span>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
