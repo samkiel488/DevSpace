e3291abdf109a91e6ae013d460fae292acfb3a17
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-24 00:53:18 +0100

    Add initial teamMembers data to team.js for team component

--- frontend/src/Componets/team.js
+++ frontend/src/Componets/team.js
@@ -1,3 +1,40 @@
-const teamMembers = [];
+const teamMembers = [
+  {
+    name: "lorem Ipsum",
+    role: "Software Developer",
+    description:
+      "Whether you're just getting started or leveling up your skills,you're in the right place. Collaborate on real projects, share ideas, and build a future fueled by code and creativity.",
+    facebookLink: "https://facebook.com",
+    githubLink: "https://github.com",
+    twitterLink: "https://x.com/",
+  },
+  {
+    name: "lorem Ipsum",
+    role: "Software Developer",
+    description:
+      "Whether you're just getting started or leveling up your skills,you're in the right place. Collaborate on real projects, share ideas, and build a future fueled by code and creativity.",
+    facebookLink: "https://facebook.com",
+    githubLink: "https://github.com",
+    twitterLink: "https://x.com/",
+  },
+  {
+    name: "lorem Ipsum",
+    role: "Software Developer",
+    description:
+      "Whether you're just getting started or leveling up your skills,you're in the right place. Collaborate on real projects, share ideas, and build a future fueled by code and creativity.",
+    facebookLink: "https://facebook.com",
+    githubLink: "https://github.com",
+    twitterLink: "https://x.com/",
+  },
+  {
+    name: "lorem Ipsum",
+    role: "Software Developer",
+    description:
+      "Whether you're just getting started or leveling up your skills,you're in the right place. Collaborate on real projects, share ideas, and build a future fueled by code and creativity.",
+    facebookLink: "https://facebook.com",
+    githubLink: "https://github.com",
+    twitterLink: "https://x.com/",
+  },
+];
 
 export default teamMembers;
