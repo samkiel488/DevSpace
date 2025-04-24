0e797176dd2baa890dc90682be45ff4bd99174bf
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-24 01:09:06 +0100

    Replace social media icons with SVG images for Facebook, GitHub, and Twitter in OurTeam component

--- frontend/src/Componets/Home/ourTeam.jsx
+++ frontend/src/Componets/Home/ourTeam.jsx
@@ -29,47 +29,26 @@ export default function OurTeam() {
                     </h3>
                     <p class="mb-4">{members.description}</p>
                     <span class="inline-flex">
-                      <a
-                        class="text-black dark:text-white"
-                        href={members.facebookLink}
-                      >
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                        </svg>
+                      <a href={members.facebookLink}>
+                        <img
+                          src="/images/svg/facebook-svgrepo-com.svg"
+                          className="h-5 w-5"
+                          alt="facebookLogo"
+                        />
                       </a>
-                      <a class="ml-2 text-black dark:text-white">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                        </svg>
+                      <a className="ml-2" href={members.githubLink}>
+                        <img
+                          src="/images/svg/github-icon-svgrepo-com.svg"
+                          alt="githubLogo"
+                          className="h-5 w-5"
+                        />
                       </a>
-                      <a class="ml-2 text-black dark:text-white">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                        </svg>
+                      <a className="ml-2" href={members.twitterLink}>
+                        <img
+                          src="/images/svg/twitter-svgrepo-com.svg"
+                          alt="twitter-svgrepo-com"
+                          className="h-5 w-5"
+                        />
                       </a>
                     </span>
                   </div>
