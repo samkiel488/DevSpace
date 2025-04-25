b79ec47a1aabaa9969c064a3e220cc9acbc40181
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-25 09:09:04 +0100

    Implement About Us component layout with Header and Footer

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,1 +1,11 @@
-export default function AboutUs() {}
+import Footer from "../Componets/footer";
+import Header from "../Componets/header";
+
+export default function AboutUs() {
+  return (
+    <>
+      <Header colorLightMode="black" colorDarkMode="black" />
+      <Footer />
+    </>
+  );
+}
