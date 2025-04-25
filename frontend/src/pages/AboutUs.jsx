d9323894e3e600bd0c6ca081ddd17780b137a8cc
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-25 19:20:32 +0100

    Refactor About Us component layout to include dark mode toggle functionality

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,11 +1,17 @@
+import AboutUsHeading from "../Componets/AboutUs/aboutUsHeading";
 import Footer from "../Componets/footer";
 import Header from "../Componets/header";
+import Toggle from "../Componets/toggle";
+import { useState } from "react";
 
 export default function AboutUs() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <>
+    <div data-theme={darkMode ? "dark" : "light"}>
       <Header colorLightMode="black" colorDarkMode="black" />
+      <AboutUsHeading />
       <Footer />
-    </>
+      <Toggle darkMode={darkMode} setDarkMode={setDarkMode} />
+    </div>
   );
 }
