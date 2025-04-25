15a0e53ed82c9d9cb5bdec7580883c073db85acb
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-25 20:52:29 +0100

    Enhance AboutUsHeading component with background colors and text adjustments for better visibility

--- frontend/src/Componets/AboutUs/aboutUsHeading.jsx
+++ frontend/src/Componets/AboutUs/aboutUsHeading.jsx
@@ -1,20 +1,20 @@
 export default function AboutUsHeading() {
   return (
-    <div class="sm:flex items-center max-w-screen-xl">
+    <div class="sm:flex items-center max-w-screen-xl bg-white  dark:bg-slate-800 text-black dark:text-white">
       <div class="sm:w-1/2 p-10">
         <div class="image object-center text-center">
-          <img src="https://i.imgur.com/WbQnbas.png" />
+          <img src="https://i.imgur.com/WbQnbas.png" className="w-300" />
         </div>
       </div>
       <div class="sm:w-1/2 p-5">
         <div class="text">
-          <span class="text-black border-b-2 border-indigo-600 uppercase">
+          <span class="text-black dark:text-white border-b-2 border-indigo-600 uppercase">
             About us
           </span>
           <h2 class="my-4 font-bold text-3xl  sm:text-4xl ">
-            About <span class="text-indigo-600">Our Company</span>
+            About <span class="text-blue-500">Our Company</span>
           </h2>
-          <p class="text-gray-700">
+          <p class="text-black dark:text-white">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid,
             commodi doloremque, fugiat illum magni minus nisi nulla numquam
             obcaecati placeat quia, repellat tempore voluptatum.
