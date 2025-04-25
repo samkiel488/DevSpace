13dd8cbedfeb21a0d676c633903c1795380a9732
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-25 08:47:08 +0100

    Add About Us route to the application router

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router";
 import App from "./pages/App";
 import Login from "./pages/login";
 import Register from "./pages/register";
+import AboutUs from "./pages/AboutUs";
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register/>}/>
+      <Route path="/register" element={<Register />} />
+      <Route path="/aboutus" element={<AboutUs />} />
     </Routes>
   );
 }
