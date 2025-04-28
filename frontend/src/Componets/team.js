64a5c59c7950ed06d3fe1fb97b617146e0f1654f
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-28 23:48:04 +0100

    Update team member name and image for accuracy in team.js

--- frontend/src/Componets/team.js
+++ frontend/src/Componets/team.js
@@ -21,14 +21,14 @@ const teamMembers = [
     imageurl: "https://devspace-ten.vercel.app/images/CyPRus.jpg",
   },
   {
-    name: "lorem Ipsum",
+    name: "khallydah",
     role: "Software Developer",
     description:
       "Whether you're just getting started or leveling up your skills,you're in the right place. Collaborate on real projects, share ideas, and build a future fueled by code and creativity.",
     facebookLink: "https://facebook.com",
     githubLink: "https://github.com",
     twitterLink: "https://x.com/",
-    imageurl: "https://picsum.photos/200",
+    imageurl: "https://devspace-ten.vercel.app/images/Khallydah.jpg",
   },
   {
     name: "lorem Ipsum",
