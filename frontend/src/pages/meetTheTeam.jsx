668d338c2f75ae4955e1fc7b6d7bb84274003a4d
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-28 13:27:54 +0100

    Implement MeetTheTeam component with Header and Footer for layout

--- frontend/src/pages/meetTheTeam.jsx
+++ frontend/src/pages/meetTheTeam.jsx
@@ -1,7 +1,11 @@
-export default function MeetTheTeam(){
-    return(
-        <>
-        
-        </>
-    )
+import Footer from "../Componets/footer";
+import Header from "../Componets/header";
+
+export default function MeetTheTeam() {
+  return (
+    <>
+      <Header colorLightMode="black" />
+      <Footer />
+    </>
+  );
 }
