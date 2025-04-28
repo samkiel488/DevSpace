c2f435b089ef404df223e176868a1f1bbe272e15
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-28 13:48:50 +0100

    Update OurTeamProfiles component layout and styling for better presentation

--- frontend/src/Componets/MeetTheTeam/ourTeamProfile.jsx
+++ frontend/src/Componets/MeetTheTeam/ourTeamProfile.jsx
@@ -1,9 +1,9 @@
 export default function OurTeamProfiles() {
   return (
     <section class="text-gray-600 body-font">
-      <div class="container px-5 py-24 mx-auto">
-        <div class="flex flex-col text-center w-full mb-20">
-          <h1 class="text-2xl font-medium title-font mb-4 text-gray-900">
+      <div class="container px-5 py-10 mx-auto">
+        <div class="flex flex-col text-center w-full mb-10">
+          <h1 class="text-2xl font-medium title-font mb-4 text-black">
             OUR TEAM
           </h1>
           <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
@@ -17,11 +17,12 @@ export default function OurTeamProfiles() {
             <div class="h-full flex flex-col items-center text-center">
               <img
                 alt="team"
+                style={{ width: "200px" }}
                 class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
                 src="https://dummyimage.com/200x200"
               />
               <div class="w-full">
-                <h2 class="title-font font-medium text-lg text-gray-900">
+                <h2 class="title-font font-medium text-lg text-black">
                   Alper Kamu
                 </h2>
                 <h3 class="text-gray-500 mb-3">UI Developer</h3>
@@ -73,7 +74,7 @@ export default function OurTeamProfiles() {
               </div>
             </div>
           </div>
-          <div class="p-4 lg:w-1/4 md:w-1/2">
+          {/* <div class="p-4 lg:w-1/4 md:w-1/2">
             <div class="h-full flex flex-col items-center text-center">
               <img
                 alt="team"
@@ -252,7 +253,7 @@ export default function OurTeamProfiles() {
                 </span>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
