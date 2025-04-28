48d53b461dbb273acab8e59d4e2ab93cad577b11
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-28 13:20:04 +0100

    Fix class attribute to className in AboutUsHeading component for React compatibility

--- frontend/src/Componets/AboutUs/aboutUsHeading.jsx
+++ frontend/src/Componets/AboutUs/aboutUsHeading.jsx
@@ -1,23 +1,23 @@
 export default function AboutUsHeading() {
   return (
-    <div class="sm:flex items-center max-w-screen-xl bg-white  dark:bg-slate-800 text-black dark:text-white">
-      <div class="sm:w-1/2 p-10">
-        <div class="image object-center text-center">
+    <div className="sm:flex items-center max-w-screen-xl bg-white  dark:bg-slate-800 text-black dark:text-white">
+      <div className="sm:w-1/2 p-10">
+        <div className="image object-center text-center">
           <img
             src="http://devspace.ct.ws/images/aboutus1.jpg"
             style={{ height: "350px" }}
           />
         </div>
       </div>
-      <div class="sm:w-1/2 p-5">
-        <div class="text">
-          <span class="text-black dark:text-white border-b-2 border-blue-500 uppercase">
+      <div className="sm:w-1/2 p-5">
+        <div className="text">
+          <span className="text-black dark:text-white border-b-2 border-blue-500 uppercase">
             About us
           </span>
-          <h2 class="my-4 font-bold text-3xl  sm:text-4xl ">
-            About <span class="text-blue-500">D3V++ Group</span>
+          <h2 className="my-4 font-bold text-3xl  sm:text-4xl ">
+            About <span className="text-blue-500">D3V++ Group</span>
           </h2>
-          <p class="text-black dark:text-white">
+          <p className="text-black dark:text-white">
             Welcome to D3V++, a passionate team of developers, designers, and
             innovators committed to building cutting-edge technology solutions.
             With expertise in software development, cybersecurity, web
