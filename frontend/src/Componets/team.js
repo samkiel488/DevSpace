da63df85b7755a6df501ff3f04638a880b1120b8
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-28 23:38:53 +0100

    Update team member name for accuracy in OurTeamProfiles component

--- frontend/src/Componets/team.js
+++ frontend/src/Componets/team.js
@@ -1,6 +1,6 @@
 const teamMembers = [
   {
-    name: "lorem Ipsum",
+    name: "ѕαмкιєℓ.∂єν",
     role: "Software Developer",
     description:
       "Whether you're just getting started or leveling up your skills,you're in the right place. Collaborate on real projects, share ideas, and build a future fueled by code and creativity.",
