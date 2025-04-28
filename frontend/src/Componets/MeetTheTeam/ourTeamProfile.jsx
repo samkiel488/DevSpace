f92fe4ecb74b8cfe36672c5f53d6af80362fa6cf
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-28 13:50:27 +0100

    Enhance OurTeamProfiles component styling for improved visibility in light and dark modes

--- frontend/src/Componets/MeetTheTeam/ourTeamProfile.jsx
+++ frontend/src/Componets/MeetTheTeam/ourTeamProfile.jsx
@@ -1,9 +1,9 @@
 export default function OurTeamProfiles() {
   return (
-    <section class="text-gray-600 body-font">
+    <section class=" bg-white  dark:bg-slate-800 text-black dark:text-white body-font">
       <div class="container px-5 py-10 mx-auto">
         <div class="flex flex-col text-center w-full mb-10">
-          <h1 class="text-2xl font-medium title-font mb-4 text-black">
+          <h1 class="text-2xl font-medium title-font mb-4 text-black dark:text-white">
             OUR TEAM
           </h1>
           <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
