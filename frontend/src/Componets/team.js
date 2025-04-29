fc14d8a3da2af1951e0ffff6716e841611847ab5
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-29 17:14:42 +0100

    Update team member names and roles for accuracy in team.js

--- frontend/src/Componets/team.js
+++ frontend/src/Componets/team.js
@@ -1,6 +1,6 @@
 const teamMembers = [
   {
-    name: "ѕαмкιєℓ.∂єν",
+    name: "Samuel Ezekiel",
     role: "FullStack Developer",
     description:
       "A versatile developer skilled in HTML, CSS, JavaScript, React, PHP, and Python, capable of building dynamic, responsive web applications from front to back.",
@@ -11,7 +11,7 @@ const teamMembers = [
       "https://devspace-ten.vercel.app/images/%D1%95%CE%B1%D0%BC%D0%BA%CE%B9%D1%94%E2%84%93.%E2%88%82%D1%94%CE%BD.jpg",
   },
   {
-    name: "CyPRus.XL",
+    name: "Bello Abdulmalik",
     role: "Frontend Developer",
     description:
       "A front-end developer skilled in HTML, CSS, and JavaScript, creating responsive, interactive websites with clean design and smooth user experiences across all devices.",
@@ -21,7 +21,7 @@ const teamMembers = [
     imageurl: "https://devspace-ten.vercel.app/images/CyPRus.jpg",
   },
   {
-    name: "khallydah",
+    name: "Atobajaye khaleed",
     role: "Frontend Developer",
     description:
       "A front-end developer skilled in HTML, CSS, JavaScript, and React, capable of building responsive, interactive user interfaces with modern design and smooth functionality.",
@@ -31,14 +31,14 @@ const teamMembers = [
     imageurl: "https://devspace-ten.vercel.app/images/Khallydah.jpg",
   },
   {
-    name: "lorem Ipsum",
-    role: "Software Developer",
+    name: "Abraham Precious Emmanuel",
+    role: "Back-end Developer",
     description:
-      "Whether you're just getting started or leveling up your skills,you're in the right place. Collaborate on real projects, share ideas, and build a future fueled by code and creativity.",
+      "A versatile developer skilled in Python, Java, and PHP, adept at building robust software solutions, automating tasks, and solving complex problems efficiently.",
     facebookLink: "https://facebook.com",
     githubLink: "https://github.com",
     twitterLink: "https://x.com/",
-    imageurl: "https://picsum.photos/200",
+    imageurl: "http://devspace.ct.ws/images/APE.jpg",
   },
 ];
 
