2698be2de936ad1d05f265fbbc9c67857235fc23
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-30 12:32:25 +0100

    Add Headers component to Contact page layout

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -1,3 +1,10 @@
+import Footer from "../Componets/footer";
+
 export default function Contact() {
-    
+    return(
+        <div>
+            <Headers colorLightMode="black" colorDarkMode="black" />
+            <Footer/>
+        </div>
+    )
 }
