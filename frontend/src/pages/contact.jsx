ed501be31ce860e148ae81fca90dbae49683042b
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-30 14:46:01 +0100

    Add Form component to Contact page

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -1,9 +1,11 @@
+import Form from "../Componets/Contact/form";
 import Footer from "../Componets/footer";
 import Header from "../Componets/header";
 export default function Contact() {
   return (
     <>
       <Header colorLightMode="black" colorDarkMode="black" />
+      <Form />
       <Footer />
     </>
   );
