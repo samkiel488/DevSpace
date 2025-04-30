53d343acca410ffd0b74f72d8381f80f240e187f
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-30 11:04:24 +0100

    Increase heading size for better visibility in OurTeamProfiles component

--- frontend/src/Componets/MeetTheTeam/ourTeamProfile.jsx
+++ frontend/src/Componets/MeetTheTeam/ourTeamProfile.jsx
@@ -5,7 +5,7 @@ export default function OurTeamProfiles() {
     <section class=" bg-white  dark:bg-slate-800 text-black dark:text-white body-font">
       <div class="container px-5 py-10 mx-auto">
         <div class="flex flex-col text-center w-full mb-10">
-          <h1 class="text-2xl font-medium title-font mb-4 text-black dark:text-white">
+          <h1 class="text-3xl font-medium title-font mb-4 text-black dark:text-white">
             OUR TEAM
           </h1>
           <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
