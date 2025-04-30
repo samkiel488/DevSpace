c86df55be4e07971e7c311113c5682c8049c10fc
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-30 15:10:25 +0100

    Refactor Contact component to include dark mode toggle functionality and improve layout structure

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -1,12 +1,17 @@
 import Form from "../Componets/Contact/form";
 import Footer from "../Componets/footer";
 import Header from "../Componets/header";
+import { useState } from "react";
+import Toggle from "../Componets/toggle";
+
 export default function Contact() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <>
+    <div data-theme={darkMode ? "dark" : "light"}>
       <Header colorLightMode="black" colorDarkMode="black" />
       <Form />
       <Footer />
-    </>
+      <Toggle darkMode={darkMode} setDarkMode={setDarkMode} />
+    </div>
   );
 }
