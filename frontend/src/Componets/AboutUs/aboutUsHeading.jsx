2caeb20d0bb8cf2d80b3e57bd42d1a9c1481517a
Areo Ayomide <areoayomide2008@gmail.com>
2025-05-01 11:55:30 +0100

    Update About Us heading component for improved layout and spacing

--- frontend/src/Componets/AboutUs/aboutUsHeading.jsx
+++ frontend/src/Componets/AboutUs/aboutUsHeading.jsx
@@ -1,7 +1,7 @@
 export default function AboutUsHeading() {
   return (
     <section class=" body-font dark:bg-slate-800 text-black dark:text-white">
-      <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <div class="container mx-auto flex px-5 py-5 md:flex-row flex-col items-center">
         <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
           <img
             class="object-cover object-center rounded"
