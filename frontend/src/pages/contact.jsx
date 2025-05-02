98339df937fd08e64cb859901d01de8af3675d6f
Areo Ayomide <areoayomide2008@gmail.com>
2025-05-02 10:58:32 +0100

    Refactor Contact component to use useLocalStorage for dark mode preference

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -1,11 +1,11 @@
 import Form from "../Componets/Contact/form";
 import Footer from "../Componets/footer";
 import Header from "../Componets/header";
-import { useState } from "react";
 import Toggle from "../Componets/toggle";
-
+import useLocalStorage from "use-local-storage";
 export default function Contact() {
-  const [darkMode, setDarkMode] = useState(false);
+  const prefences = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", prefences);
   return (
     <div data-theme={darkMode ? "dark" : "light"}>
       <Header colorLightMode="black" colorDarkMode="black" />
