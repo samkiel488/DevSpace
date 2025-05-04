e079fc346a39b622da3e75a541c7b0d4c3751f53
Areo Ayomide <areoayomide2008@gmail.com>
2025-05-04 14:54:51 +0100

    Add 404 page component with header and footer

---
+++ frontend/src/pages/404page.jsx
@@ -0,0 +1,11 @@
+import Footer from "../Componets/footer";
+import Header from "../Componets/header";
+
+export default function Page404() {
+  return (
+    <>
+      <Header colorLightMode="black" colorDarkMode="black" />
+      <Footer />
+    </>
+  );
+}
