29906916632587d3a947fad3e123fc8e26078376
Areo Ayomide <areoayomide2008@gmail.com>
2025-05-07 11:11:08 +0100

    Redirect to login page on error fetching post instead of register

--- frontend/src/pages/userLoggedIn/home.jsx
+++ frontend/src/pages/userLoggedIn/home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
           }
         } catch (error) {
           console.error("Error fetching post:", error);
-          navigate("/register");
+          navigate("/login");
         }
       };
       fetchPost();
