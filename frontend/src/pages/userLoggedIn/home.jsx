bd56f984edd9bffb07f72c57009cfe3296f1f29b
Areo Ayomide <areoayomide2008@gmail.com>
2025-05-12 15:51:54 +0100

    Refactor Home component to use HomeMain for displaying recent posts

--- frontend/src/pages/userLoggedIn/home.jsx
+++ frontend/src/pages/userLoggedIn/home.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import Cookies from "js-cookie";
+import HomeMain from "../../Componets/Dashboard/Home/homeMain";
 
 const url = import.meta.env.VITE_API_URL;
 
@@ -85,58 +86,7 @@ const Home = () => {
             <LeftSideBar userName={userName} />
 
             {/* Main content */}
-            <main className="lg:col-span-7  xl:col-span-10">
-              <div className="mt-4">
-                <h1 className="text-2xl font-bold text-gray-900 text-center">
-                  Recent Posts
-                </h1>
-                <ul role="list" className="space-y-4">
-                  {post.map((item) => {
-                    return (
-                      <li className="bg-white px-4 py-6 m-5 shadow sm:rounded-lg sm:px-6">
-                        <article aria-labelledby="question-title-81614">
-                          <div>
-                            <div className="flex space-x-3">
-                              <div className="flex-shrink-0">
-                                <img
-                                  className="h-10 w-10 rounded-full"
-                                  src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                  alt=""
-                                />
-                              </div>
-                              <div className="min-w-0 flex-1">
-                                <p className="text-sm font-medium text-gray-900">
-                                  <a href="#" className="hover:underline">
-                                    {item.name}
-                                  </a>
-                                </p>
-                                <p className="text-sm text-gray-500">
-                                  <a href="#" className="hover:underline">
-                                    <time dateTime="2020-12-09T11:43:00">
-                                      {item.date}
-                                    </time>
-                                  </a>
-                                </p>
-                              </div>
-                            </div>
-                            <h2
-                              id="question-title-81614"
-                              className="mt-4 text-base font-medium text-gray-900"
-                            >
-                              {item.postHeadline}
-                            </h2>
-                          </div>
-                          <div className="mt-2 space-y-4 text-sm text-gray-700">
-                            <p>{item.postText}</p>
-                          </div>
-                          <div className="mt-6 flex justify-between space-x-8"></div>
-                        </article>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
-            </main>
+            <HomeMain post={post} />
           </div>
         </div>
       </div>
