ed082f0e94b373b858850463589f25dc8edec329
Areo Ayomide <areoayomide2008@gmail.com>
2025-05-13 11:57:14 +0100

    Update CORS options to allow additional HTTP methods

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 8080;
 const corsOptions = {
   origin: "http://localhost:5173",
-  methods: ["GET", "POST"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 };
 
 app.use(cors(corsOptions));
