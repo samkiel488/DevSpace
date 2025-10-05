f511ce56d14c7d9fc4a72df50a3e44c7a0931a0f
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-10-06 00:14:55 +0100

    updating my project route

--- Backend/controller/project.controller.js
+++ Backend/controller/project.controller.js
@@ -2,8 +2,8 @@ import Project from "../models/project.models.js";
 
 export async function getAllProjects(req, res, next) {
   try {
-    const project = await Project.find().populate("user","_id name username");
-    return res.status(200).json({success: true, data:{project}});
+    const project = await Project.find().populate("user", "_id name username");
+    return res.status(200).json({ success: true, data: { project } });
   } catch (err) {
     next(err);
   }
@@ -70,6 +70,118 @@ export async function postNewProject(req, res, next) {
         .status(400)
         .json({ success: false, error: "Duplicate titles" });
     }
+    next(err);
+  }
+}
+
+export async function getProjectById(req, res, next) {
+  try {
+    const { id } = req.params;
+
+    const project = await Project.findById(id).populate(
+      "user",
+      "_id name username"
+    );
+
+    if (!project) {
+      return res
+        .status(404)
+        .json({ success: false, error: "Project Not Found" });
+    }
+
+    return res.status(200).json({ success: true, data: { project } });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function deleteProjectById(req, res, next) {
+  try {
+    const { id } = req.params;
+    console.log(id);
+
+    const project = await Project.deleteOne({ _id: id });
+
+    if (!project.acknowledged) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Unable to delete Project" });
+    }
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Project deleted Successfully" });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function editProjectById(req, res, next) {
+  try {
+    const { id } = req.params;
+    const { title, description, category, imageUrl, codeUrl, previewUrl } =
+      req.body;
+
+    const project = await Project.findById(id);
+    if (!project) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Project doesn't exist" });
+    }
+
+    if (
+      title === project.title &&
+      description === project.description &&
+      category === project.category &&
+      codeUrl === project.codeUrl &&
+      previewUrl === project.previewUrl &&
+      imageUrl === project.imageUrl
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, error: "No changes was made" });
+    }
+
+      let editing = true;
+
+      if (title !== project.title){
+        project.title = title;
+        editing = false;
+    }
+
+      if(description !== project.description){
+          project.description = description;
+          editing = false;
+      }
+
+      if(category !== project.category){
+          project.category = category;
+          editing = false;
+      }
+
+      if(codeUrl !== project.codeUrl){
+          project.codeUrl = codeUrl;
+          editing = false;
+      }
+
+      if(previewUrl !== previewUrl){
+          project.previewUrl = previewUrl;
+          editing = false;
+      }
+
+      if(imageUrl !== imageUrl){
+          project.imageUrl = imageUrl;
+          editing = false;
+      }
+
+      if(editing){
+          return res.status(200).json({ success: false, error: "No input was changed" });
+      }
+
+      await project.save();
+
+      return res.status(200).json({ success: true, data: { project } });
+  } catch (err) {
     next(err);
   }
 }
--- Backend/router/project.router.js
+++ Backend/router/project.router.js
@@ -1,10 +1,19 @@
 import {Router} from "express";
 import {ProtectMiddleware} from "../middleware/protect.middleware.js";
-import {getAllProjects, postNewProject} from "../controller/project.controller.js";
+import {
+    deleteProjectById,
+    editProjectById,
+    getAllProjects,
+    getProjectById,
+    postNewProject
+} from "../controller/project.controller.js";
 
 const projectRouter = Router();
 
 projectRouter.get("/",getAllProjects);
 projectRouter.post("/", ProtectMiddleware,postNewProject);
+projectRouter.get("/:id", ProtectMiddleware,getProjectById);
+projectRouter.patch("/:id", ProtectMiddleware,editProjectById);
+projectRouter.delete("/:id", ProtectMiddleware, deleteProjectById);
 
 export default projectRouter;
