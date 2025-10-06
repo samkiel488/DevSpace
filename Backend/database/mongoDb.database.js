ce104808a04ab1b43fae34bea820cd7042a63ada
samkiel488 <samuelezekiel488@gmail.com>
2025-10-06 02:39:06 +0100

    Update connection success message in MongoDB database file

--- Backend/database/mongoDb.database.js
+++ Backend/database/mongoDb.database.js
@@ -4,7 +4,7 @@ import { DATABASE_URI } from "../config/env.config.js";
 export async function connectToMongoDB() {
   try {
     await mongoose.connect(DATABASE_URI);
-    console.log("Connected Successfully to Mongo DB");
+    console.log("Server is now Connected");
   } catch (error) {
     console.log(error);
     process.exit(1);
