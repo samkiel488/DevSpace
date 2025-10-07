38e5bf1ec59994b39dac19ef4507a4e7df20b77f
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-10-07 22:51:13 +0100

    Add share button to settings layout and implement clipboard copy functionality

--- frontend/src/Componets/Dashboard/settingsLayout.jsx
+++ frontend/src/Componets/Dashboard/settingsLayout.jsx
@@ -1,5 +1,13 @@
-import { Github, Instagram, Link, Linkedin, Twitter } from "lucide-react";
+import {
+  Github,
+  Instagram,
+  Link,
+  Linkedin,
+  Share,
+  Twitter,
+} from "lucide-react";
 import { Outlet, useLoaderData, useRouteLoaderData } from "react-router";
+import copyToClipboard from "../../libs/copyLinktoClipboard";
 
 export default function SettingLayout() {
   const {
@@ -28,7 +36,21 @@ export default function SettingLayout() {
         <div className="mt-10 p-2 md:mt-0 md:pl-47">
           <div className="flex w-full px-2 flex-col sm:flex-row">
             <div className="flex flex-col flex-wrap items-center w-full sm:items-start">
-              <h1 className="text-2xl font-semibold">{name}</h1>
+              <div className="flex gap-3 justify-center items-center">
+                <h1 className="text-2xl font-semibold">{name}</h1>
+                <button
+                  type="button"
+                  className="dark-rounded-button !p-1"
+                  target="_blank"
+                  onClick={async () => {
+                    copyToClipboard({
+                      path: `share/${profile?._id}`,
+                    });
+                  }}
+                >
+                  <Share className="w-4 h-4" />
+                </button>
+              </div>
               <p className="font-[350]">{profile?.role}</p>
             </div>
             <div className="flex justify-center sm:justify-end-safe gap-3 p-2">
