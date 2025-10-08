6ca5a62690daed82260db341f84f9f82e96ec115
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-10-08 15:50:54 +0100

    Add Recent Projects section to ProfileId page

--- frontend/src/pages/dashboard/profile/profileId.jsx
+++ frontend/src/pages/dashboard/profile/profileId.jsx
@@ -4,6 +4,7 @@ import { useLoaderData } from "react-router";
 import Bio from "../../../Componets/Dashboard/bio";
 import SettingGrid from "../../../Componets/Dashboard/settingGrid";
 import AboutMe from "../../../Componets/Dashboard/aboutMe";
+import ProjectCard from "../../../Componets/projectCard";
 
 export default function ProfileId() {
   const { profile } = useLoaderData();
@@ -16,6 +17,17 @@ export default function ProfileId() {
       <SettingGrid profile={profile?.profile}>
         <AboutMe profile={profile?.profile} />
       </SettingGrid>
+      <div className="py-4 px-3 sm:px-10">
+        <h1 className="font-semibold text-2xl text-center mb-5">
+          Recent Projects
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-col-2 md:grid-cols-3 gap-4 gap-y-10">
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
+      </div>
     </div>
   );
 }
